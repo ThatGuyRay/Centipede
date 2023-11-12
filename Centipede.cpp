34c9ea86ab9e93eca335d447efc4caf5b190925b
Rayyan Waqar <destroyt.876@gmail.com>
2023-11-12 13:52:14 +0500

    Created Player Square

--- Centipede.cpp
+++ Centipede.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 using namespace std;
 
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "Centipede");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Centipede");
 
     while (window.isOpen())
     {
@@ -16,6 +17,16 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        sf::RectangleShape player(sf::Vector2f(30, 30));
+        player.setFillColor(sf::Color::Red);
+        player.setPosition(385, 570);
+        window.draw(player);
+
+        // end the current frame
+        window.display();
     }
     return 0;
 }
