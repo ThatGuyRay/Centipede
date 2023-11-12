af5cc062b23ad2a7173844505fdb387c95135d00
Rayyan Waqar <destroyt.876@gmail.com>
2023-11-12 14:45:14 +0500

    added variable of size

--- Centipede.cpp
+++ Centipede.cpp
@@ -3,9 +3,12 @@
 #include <SFML/Graphics.hpp>
 using namespace std;
 
+const int resolutionX = 960;
+const int resolutionY = 960;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Centipede");
+    sf::RenderWindow window(sf::VideoMode(resolutionX, resolutionY), "Centipede");
 
     sf::RectangleShape player(sf::Vector2f(30, 30));
     player.setFillColor(sf::Color::Red);
@@ -26,23 +29,25 @@ int main()
         // draw everything here...
 
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) && player.getPosition().x >= 0)
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) && player.getPosition().x > 0)
         {
             player.move(-1, 0);
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && player.getPosition().x <= 770)
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && player.getPosition().x < resolutionX - player.getSize().x)
         {
             player.move(1, 0);
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) && player.getPosition().y >= 450)
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) && player.getPosition().y > 450)
         {
             player.move(0, -1);
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) && player.getPosition().y <= 570)
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) && player.getPosition().y < resolutionY - player.getSize().y)
         {
             player.move(0, 1);
         }
         window.draw(player);
+
+        
 
         // end the current frame
         window.display();
