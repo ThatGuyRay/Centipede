4dfa505a5e18c6189431e31f5039f752eb20074d
Rayyan Waqar <destroyt.876@gmail.com>
2023-11-12 14:08:10 +0500

    Created Player outside loop & Created Movement

--- Centipede.cpp
+++ Centipede.cpp
@@ -7,6 +7,10 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Centipede");
 
+    sf::RectangleShape player(sf::Vector2f(30, 30));
+    player.setFillColor(sf::Color::Red);
+    player.setPosition(385, 570);
+
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
@@ -20,9 +24,24 @@ int main()
         window.clear(sf::Color::Black);
 
         // draw everything here...
-        sf::RectangleShape player(sf::Vector2f(30, 30));
-        player.setFillColor(sf::Color::Red);
-        player.setPosition(385, 570);
+
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+        {
+            player.move(-1, 0);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+        {
+            player.move(1, 0);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+        {
+            player.move(0, -1);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+        {
+            player.move(0, 1);
+        }
         window.draw(player);
 
         // end the current frame
